e6764661bd0c2b70248d0decb28186142552d412
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-11-06 20:45:17 -0500

    used bottom tabs

--- src/stacks/root.tsx
+++ src/stacks/root.tsx
@@ -1,7 +1,11 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {ProfileScreen} from '../screens/ProfileScreen';
 import {ExploreScreenStack} from './explore';
 import {RestaurantsScreenStack} from './restaurants';
+import {ExploreIcon} from '../assets/icons/ExploreIcon';
+import {RestaurantsIcon} from '../assets/icons/RestaurantsIcon';
+import {ProfileIcon} from '../assets/icons/ProfileIcon';
 
 export type RootStackParams = {
   ExploreStack: undefined;
@@ -12,7 +16,7 @@ export type RootStackParams = {
   };
 };
 
-export const RootStack = createNativeStackNavigator<RootStackParams>();
+export const RootStack = createBottomTabNavigator<RootStackParams>();
 
 export const RootScreenStack = () => {
   return (
@@ -20,13 +24,39 @@ export const RootScreenStack = () => {
       initialRouteName="ExploreStack"
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
       }}>
-      <RootStack.Screen name="ExploreStack" component={ExploreScreenStack} />
+      <RootStack.Screen
+        name="ExploreStack"
+        component={ExploreScreenStack}
+        options={{
+          tabBarLabel: 'Explore',
+          tabBarIcon: ({color, size}) => (
+            <ExploreIcon color={color} size={size} />
+          ),
+        }}
+      />
       <RootStack.Screen
         name="RestaurantsStack"
         component={RestaurantsScreenStack}
+        options={{
+          tabBarLabel: 'Restaurants',
+          tabBarIcon: ({color, size}) => (
+            <RestaurantsIcon color={color} size={size} />
+          ),
+        }}
       />
-      <RootStack.Screen name="Profile" component={ProfileScreen} />
+      <RootStack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({color, size}) => (
+            <ProfileIcon color={color} size={size} />
+          ),
+        }}
+      />
     </RootStack.Navigator>
   );
 };
